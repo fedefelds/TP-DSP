bba301229ca8154c6287d635ec3fc7e0774c92e2
fedefelds <fedefelds@hotmail.com>
2017-06-06 05:42:28 -0300

    renamed extraer_silencio to split

--- separar.py
+++ separar.py
@@ -1,4 +1,4 @@
-# este codigo define la funcion "extraer_silencio". La misma se basa en librosa.effects.split
+# este codigo define la funcion "separar". La misma se basa en librosa.effects.split
 
 ##################################################
 
@@ -29,19 +29,23 @@
 
 import librosa
 import numpy as np
-from cargar_audio import cargar_audio # todo:  si no escribo esto asi, me da 'module object is not callable'
+from cargar_audio import cargar_audio  # todo:  si no escribo esto asi, me da 'module object is not callable'
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 mpl.rcParams['agg.path.chunksize'] = 10000
 
-def extraer_silencio(y, top_db=60, ref=np.amax, frame_length=2048, hop_length=512):
-    y,sr = cargar_audio('/Users/Fede/Documents/Github/TP-DSP/07_Calling_All_My_Lovelies.wav',None,True,0,None,np.float32,'kaiser_best')
-    intervals=librosa.effects.split(y, top_db, ref, frame_length , hop_length)
+filename='/Users/Fede/Documents/Github/TP-DSP/prueba.wav'
 
+def separar(filename, top_db=60, ref=np.amax, frame_length=1024, hop_length=50):
+    y,sr = cargar_audio(filename,None,True,0,None,np.float32,'kaiser_best')
+    intervals=librosa.effects.split(y, top_db, ref, frame_length , hop_length)
     return intervals
+
 
 ##################################################
 # Ejemplo:
-# y,sr = cargar_audio('/Users/Fede/Documents/Github/TP-DSP/07_Calling_All_My_Lovelies.wav',None,True,0,None,np.float32,'kaiser_best')
-# intervalos=extraer_silencio(y)
+# intervalos=separar(filename,hop_length=50)
+# plt.plot()
+# print(intervalos[0])
+# librosa.output.write_wav('file_trim_5s.wav',intervalo1, sr)
 ##################################################
