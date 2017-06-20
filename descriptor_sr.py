62807fda209ab3f458b02857f28045b3df91db30
fedefelds <fedefelds@hotmail.com>
2017-06-19 21:08:34 -0300

    add sr descriptor

--- descriptor_sr.py
+++ descriptor_sr.py
@@ -1,11 +1,24 @@
 import librosa
 import numpy as np
-filename='/Users/Fede/Desktop/stew.mp3'
+carpeta='/Users/Fede/Desktop/'
+filename='copia de burno mars'
+formato='.mp3'
+filename=carpeta+filename+formato
+# cargar audio
 y, sr = librosa.load(filename)
-intervals=librosa.effects.split(y, top_db=60, ref=np.amax, frame_length=1024, hop_length=50)
+# extraer silencios al principio y final
+y,index=librosa.effects.trim(y, top_db=60, ref=np.amax,
+frame_length=1024, hop_length=50)
+# normalizar
+valor_max=np.max(y)
+y=y/valor_max
+# calcular sr
+intervals=librosa.effects.split(y, top_db=60, ref=np.amax,
+frame_length=1024, hop_length=50)
 M_s=0
 for i in range(0,intervals.shape[0]):
     M_s = M_s+intervals[i,1]-intervals[i,0]
 N=len(y)
 SR=((N-M_s)/N)
-print(SR)
+SR=np.array([SR])
+np.savetxt('SR'+'.txt',chromagram,delimiter=',',fmt='%.3f', newline=' & ')
