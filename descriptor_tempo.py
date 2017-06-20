46dbbbf514b00cb30463422abfc38a82b71cf00e
fedefelds <fedefelds@hotmail.com>
2017-06-19 21:09:12 -0300

    add tempo descriptor

--- descriptor_tempo.py
+++ descriptor_tempo.py
@@ -1,14 +1,25 @@
 import librosa
 import numpy as np
-# Compute local onset autocorrelation
-y, sr = librosa.load('/Users/Fede/Desktop/stew.mp3')
+
+carpeta='/Users/Fede/Desktop/The Turn of a Friendly Card 1979 (GPF)/Canciones del trabajo/'
+filename='10'
+formato='.mp3'
+filename=carpeta+filename+formato
+# cargar audio
+y, sr = librosa.load(filename)
+# extraer silencios al principio y final
+y,index=librosa.effects.trim(y, top_db=60, ref=np.amax, frame_length=2048, hop_length=50)
+# normalizar
+valor_max=np.max(y)
+y=y/valor_max
+# calcular tempo
 hop_length = 512
 oenv = librosa.onset.onset_strength(y=y, sr=sr, hop_length=hop_length)
 tempogram = librosa.feature.tempogram(onset_envelope=oenv, sr=sr,
                                       hop_length=hop_length)
-# Compute global onset autocorrelation
 ac_global = librosa.autocorrelate(oenv, max_size=tempogram.shape[0])
 ac_global = librosa.util.normalize(ac_global)
 # Estimate the global tempo for display purposes
 tempo = librosa.beat.tempo(onset_envelope=oenv, sr=sr,hop_length=hop_length)[0]
 print(tempo)
+# np.savetxt('tempo'+'filename'+'.txt',chromagram,delimiter=',',fmt='%.3f', newline=' & ')
