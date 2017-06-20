e15c0e25100219ea9a168d387a1fc78125580ea3
fedefelds <fedefelds@hotmail.com>
2017-06-19 21:08:09 -0300

    add cresta descriptor

---
+++ descriptor_cresta.py
@@ -0,0 +1,25 @@
+import librosa
+import numpy as np
+import matplotlib.pyplot as plt
+carpeta='/Users/Fede/Desktop/'
+filename='copia de burno mars'
+formato='.mp3'
+filename=carpeta+filename+formato
+# cargar audio
+y, sr = librosa.load(filename)
+# extraer silencios al principio y final
+y,index=librosa.effects.trim(y, top_db=60, ref=np.amax, frame_length=2048, hop_length=50)
+# normalizar
+valor_max=np.max(y)
+y=y/valor_max
+# calculo el valor rms
+y_rms=y*y
+y_rms=np.sum(y_rms)
+y_rms=y_rms/len(y)
+y_rms=np.sqrt(y_rms)
+# calculo el valor maximo
+y_max=np.max(y) # siempre vale 1
+# calculo el factor de cresta
+factor_de_cresta=((y_max)/(y_rms))
+
+
