a00172735e7087fcc8b185b398a2720f881273c5
fedefelds <fedefelds@hotmail.com>
2017-06-21 20:55:38 -0300

    elimino archivos no usados

--- normalizar.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def normalizar(senal_a_normalizar):
-    valor_max=np.max(senal_a_normalizar)
-    senal_normalizada=senal_a_normalizar/valor_max
-    return senal_normalizada
-
-
