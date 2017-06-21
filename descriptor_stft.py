e2c793fc30ef2c00569ba852c34c6a29c87584e0
fedefelds <fedefelds@hotmail.com>
2017-06-21 15:01:55 -0300

    termino el tp

--- descriptor_stft.py
+++ descriptor_stft.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 carpeta='/Users/Fede/Desktop/'
 filename='burno mars'
 formato='.mp3'
-filename=carpeta+filename+formato
+filename='/Users/Fede/Desktop/The Turn of a Friendly Card 1979 (GPF)/440.wav'
 # cargar audio
 y, sr = librosa.load(filename)
 # extraer silencios al principio y final
@@ -16,7 +16,7 @@ y,index=librosa.effects.trim(y, top_db=60, ref=np.amax, frame_length=2048, hop_l
 # normalizar
 valor_max=np.max(y)
 y=y/valor_max
-D = librosa.stft(y)
+D = librosa.stft(y,win_length=3000)
 librosa.display.specshow(librosa.amplitude_to_db(D,ref=np.max)
 ,y_axis='log', x_axis='time')
 plt.title('Power spectrogram')
